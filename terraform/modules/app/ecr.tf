c164b11d380cf46c31923a96376487b311ca2fad
austinloveless <austin.loveless@contino.io>
2023-05-26 11:52:58 -0600

    deploy ecr without env tag

--- terraform/modules/app/ecr.tf
+++ terraform/modules/app/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "repo" {
-  name                 = "ecr-repo-${var.company}-${var.env}"
+  name                 = "ecr-repo-${var.company}"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
