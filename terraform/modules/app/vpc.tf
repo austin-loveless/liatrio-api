5275f173bbbed6284d9b61b4ba8e3ef7ebec2df1
austinloveless <austin.loveless@contino.io>
2023-05-27 23:10:42 -0600

    update tags to use correct eks name

--- terraform/modules/app/vpc.tf
+++ terraform/modules/app/vpc.tf
@@ -13,17 +13,17 @@ module "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/eks-vpc-${var.company}" = "shared"
+    "kubernetes.io/cluster/${module.eks.name}" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/eks-vpc-${var.company}" = "shared"
-    "kubernetes.io/role/elb"                       = "1"
+    "kubernetes.io/cluster/${module.eks.name}"  = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/eks-vpc-${var.company}" = "shared"
-    "kubernetes.io/role/internal-elb"              = "1"
+    "kubernetes.io/cluster/${module.eks.name}" = "shared"
+    "kubernetes.io/role/internal-elb"          = "1"
   }
 }
 
