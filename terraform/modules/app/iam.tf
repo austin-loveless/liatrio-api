97dd11a565b25629de06fed011d8f122c7872479
austinloveless <austin.loveless@contino.io>
2023-05-29 20:38:04 -0600

    terraform oidc rule for eks load balancer

--- terraform/modules/app/iam.tf
+++ terraform/modules/app/iam.tf
@@ -1,4 +1,35 @@
-# resource "aws_iam_role" "eks_load_balancer_controller_role" {
-#   name               = "EKSLoadBalancerControllerRole"
-#   assume_role_policy = file("${path.module}/policy.json")
-# }
+resource "aws_iam_role" "eks_load_balancer_controller_role" {
+  name               = "EKSLoadBalancerControllerRole-${var.env}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+           "sts:AssumeRoleWithWebIdentity",
+        ]
+        Effect   = "Allow"
+        Principal = {
+            "Federated": "arn:aws:iam::807230335956:${module.eks.oidc_provider}"
+        }
+        Condition = {
+            StringEquals = {
+                "${module.eks.oidc_provider}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller",
+                 "${module.eks.oidc_provider}:aud":  "sts.amazonaws.com"
+            }
+        }
+      },
+    ]
+  })
+}
+
+
+
+resource "aws_iam_policy" "eks_load_balancer_controller_policy" {
+  name        = "EKSLoadBalancerControllerPolicy-${var.env}"
+  policy       = file("${path.module}/policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "eks_load_balancer_controller_policy-attachment" {
+  role       = aws_iam_role.eks_load_balancer_controller_role.name
+  policy_arn = aws_iam_policy.eks_load_balancer_controller_policy.arn
+}
